6719e6b3e8127c28b583ea5e84aedfedccbd491c
tkishiya <tkishiya@R-MBP14JBN-0720.local>
2023-05-27 01:26:09 +0900

    Aboutを作成

---
+++ src/components/organisms/About/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import style from "./style.module.scss";
+
+const About: React.FC = () => {
+  return (
+    <div className={style.About}>
+      <p className={style.About__text}>
+        この場所は、自分の2022を振り返り
+        <br />
+        2023年は何をするか、どうなりたいか
+        <br />
+        一年の区切りを使って自分を知ってもらうために制作しました
+      </p>
+      <p className={style.About__textEnglish}>
+        I designed this website for self produce with my life through last year.
+        <br />
+        I’ll look back 2022 and consider what I do in 2023 from here.
+      </p>
+    </div>
+  );
+};
+
+export default About;
