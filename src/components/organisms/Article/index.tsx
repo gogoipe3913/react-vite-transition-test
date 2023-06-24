a83bcf2658ad7cfaaad051adec42dcdc7e2eea49
Taiki Kishiyama <tkishiya@MacBook-Pro.local>
2023-06-24 12:38:30 +0900

    articleページ追加

--- src/components/organisms/Article/index.tsx
+++ src/components/organisms/Article/index.tsx
@@ -15,43 +15,49 @@ const Article: React.FC<ArticleProps> = ({ articleType, className = "" }) => {
 
   return (
     <div className={classNames(style.Article, className)}>
-      <img
-        width="100%"
-        src={item.mainImage.url}
-        alt={item.mainImage.alt}
-        className={style.Article__mainImage}
-      />
+      <div className={style.Article__topContainer}>
+        <img
+          width="100%"
+          height="100%"
+          src={item.mainImage.url}
+          alt={item.mainImage.alt}
+          className={style.Article__mainImage}
+        />
+        <p className={style.Article__scrollIndicator}>scroll</p>
+      </div>
       <Link to="/" className={style.Article__linkToIndex}>
         <span className={style.Article__linkArrowAngle} />
         <span className={style.Article__linkArrow} />
-        <span>Back to index</span>
+        <span className={style.Article__linkText}>Back to index</span>
       </Link>
       <h1 className={style.Article__titleVertical}>
         <span className={style.Article__titleVerticalBody}>{item.title}</span>
         <span className={style.Article__titleVerticalMonth}>{item.month}</span>
       </h1>
-      <p className={style.Article__scrollIndicator}>scroll</p>
-      <div className={style.Article__content}>
-        <div className={style.Article__leftColumn}>
-          <p className={style.Article__title}>{item.title}</p>
-          <p className={style.Article__date}>{item.date}</p>
-          <p className={style.Article__text}>{item.text}</p>
-          <p className={style.Article__tag}>
-            <span />
-            <span className={style.Article__tagBody}>{item.tag}</span>
-            <span />
-          </p>
-          <p className={style.Article__textEnglish}>{item.textEnglish}</p>
+
+      <div className={style.Article__contentWrapper}>
+        <div className={style.Article__content}>
+          <div className={style.Article__leftColumn}>
+            <p className={style.Article__title}>{item.title}</p>
+            <p className={style.Article__date}>{item.date}</p>
+            <p className={style.Article__text}>{item.text}</p>
+            <p className={style.Article__tag}>
+              <span />
+              <span className={style.Article__tagBody}>{item.tag}</span>
+              <span />
+            </p>
+            <p className={style.Article__textEnglish}>{item.textEnglish}</p>
+          </div>
+          <ul className={style.Article__rightColumn}>
+            {item.imageFigures.map((imageFigure, index) => (
+              <li className={style.Article__imageFigure} key={index}>
+                <img width="100%" src={imageFigure.url} alt={imageFigure.alt} />
+              </li>
+            ))}
+          </ul>
         </div>
-        <ul className={style.Article__rightColumn}>
-          {item.imageFigures.map((imageFigure, index) => (
-            <li className={style.Article__imageFigure} key={index}>
-              <img width="100%" src={imageFigure.url} alt={imageFigure.alt} />
-            </li>
-          ))}
-        </ul>
+        <Footer className={style.Article__footer} />
       </div>
-      <Footer />
     </div>
   );
 };
