b5cbcae9cfb230cccc02ddbae121afbd6b959c04
Taiki Kishiyama <taiki_kishiyama@Taikis-MacBook-Pro.local>
2023-06-30 03:31:44 +0900

    タイトル周り修正

--- src/components/organisms/Top/index.tsx
+++ src/components/organisms/Top/index.tsx
@@ -6,20 +6,27 @@ const Top: React.FC = () => {
   return (
     <div className={style.Top}>
       <div className={style.Top__videoWrapper}>
+        <p className={style.Top__titleBox}>
+          <span className={style.Top__title}>LOOKBACK ON LASTYEAR</span>
+        </p>
         <div className={style.Top__videoFrame}>
           <video
             muted={true}
             autoPlay={true}
             loop={true}
-            src="../../../../public/video/topVideo.mov"
+            src="../../../../public/video/topVideoPattern3.mp4"
             className={style.Top__video}
           />
+          <div className={style.Top__titleBoxOnImage}>
+            <span className={style.Top__titleOnImage}>
+              LOOKBACK ON LASTYEAR
+            </span>
+            <span className={style.Top__titleOnImageSubTitle}>
+              + hello 2023, what i do?
+            </span>
+          </div>
         </div>
       </div>
-      <p className={style.Top__titleBox}>
-        <span className={style.Top__title}>LOOK BACK ON LAST YEAR</span>
-        <span className={style.Top__subTitle}>+ hello 2023, what i do?</span>
-      </p>
       <ScrollIndicator className={style.Top__scrollIndicator} />
     </div>
   );
